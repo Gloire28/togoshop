c00be75acccd2783a3d975b0d6f23a30913c3e8b
Gloire28 <gloiredakla28@gmail.com>
2026-08-07 03:14:33 +0000

    fix: supermarketId en String dans Order

--- togoshop-backend/src/models/Order.js
+++ togoshop-backend/src/models/Order.js
@@ -39,7 +39,7 @@ const orderSchema = new mongoose.Schema({
     required: true,
   },
   supermarketId: {
-    type: mongoose.Schema.Types.ObjectId,
+    type: String,
     ref: 'Supermarket',
     required: false,
   },
